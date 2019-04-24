883aa577a51af907586c352ba50ed78d0c46fef3
davood-dorostkar <dado2hacker@gmail.com>
2019-04-24 16:16:42 +0430

    works fine, wanna clean the code

--- 10-TFT/TFT_Touchscreen/src/main.cpp
+++ 10-TFT/TFT_Touchscreen/src/main.cpp
@@ -3,25 +3,25 @@
 #include <Adafruit_GFX.h>
 #include <MCUFRIEND_kbv.h>
 #include <TouchScreen.h>
-//==========================================================================================
+
 MCUFRIEND_kbv TFT;
 
 Adafruit_GFX_Button redButton, blueButton, greenButton, yellowButton;
 
-#define MINPRESSURE 200
-#define MAXPRESSURE 1000
+#define minPressure 200
+#define maxPressure 1000
 const int XP = 6, XM = A2, YP = A1, YM = 7;
 const int touchLeftLimit = 107, touchhRightLimit = 910, touchUpLimit = 954, touchButtomLimit = 100;
 TouchScreen touch = TouchScreen(XP, YP, XM, YM, 300);
 int touchX, touchY;
-bool Touch_getXY(void)
+bool touchSense(void)
 {
     TSPoint touchPoint = touch.getPoint();
     pinMode(YP, OUTPUT);
     pinMode(XM, OUTPUT);
     digitalWrite(YP, HIGH);
     digitalWrite(XM, HIGH);
-    bool pressed = (touchPoint.z > MINPRESSURE && touchPoint.z < MAXPRESSURE);
+    bool pressed = (touchPoint.z > minPressure && touchPoint.z < maxPressure);
     if (pressed)
     {
         touchX = map(touchPoint.x, touchLeftLimit, touchhRightLimit, 0, TFT.width());
@@ -70,45 +70,55 @@ void setup()
     makeIcons();
 }
 
+void setBackColor(int color)
+{
+    TFT.fillScreen(color);
+    makeIcons();
+}
 void loop()
 {
+    redButton.press(touchSense() && redButton.contains(touchX, touchY));
+    blueButton.press(touchSense() && blueButton.contains(touchX, touchY));
+    greenButton.press(touchSense() && greenButton.contains(touchX, touchY));
+    yellowButton.press(touchSense() && yellowButton.contains(touchX, touchY));
+
+    if (redButton.justReleased())
+    {
+        redButton.drawButton();
+    }
+    if (redButton.justPressed())
+    {
+        redButton.drawButton(true);
+        setBackColor(RED);
+    }
+
+    if (blueButton.justReleased())
+    {
+        blueButton.drawButton();
+    }
+    if (blueButton.justPressed())
+    {
+        blueButton.drawButton(true);
+        setBackColor(BLUE);
+    }
+
+    if (greenButton.justReleased())
+    {
+        greenButton.drawButton();
+    }
+    if (greenButton.justPressed())
+    {
+        greenButton.drawButton(true);
+        setBackColor(GREEN);
+    }
+
+    if (yellowButton.justReleased())
+    {
+        yellowButton.drawButton();
+    }
+    if (yellowButton.justPressed())
+    {
+        yellowButton.drawButton(true);
+        setBackColor(YELLOW);
+    }
 }
-// void setup(void)
-// {
-
-//     uint16_t ID = TFT.readID();
-
-//     TFT.begin(ID);
-//     TFT.setRotation(0);
-//     TFT.fillScreen(BLACK);
-//     on_btn.initButton(&TFT, 60, 200, 100, 40, WHITE, CYAN, BLACK, "ON", 2);
-//     off_btn.initButton(&TFT, 180, 200, 100, 40, WHITE, CYAN, BLACK, "OFF", 2);
-//     on_btn.drawButton(false);
-//     off_btn.drawButton(false);
-//     TFT.fillRect(40, 80, 160, 80, RED);
-// }
-
-// /* two buttons are quite simple
-//  */
-// void loop(void)
-// {
-//     bool down = Touch_getXY();
-//     on_btn.press(down && on_btn.contains touchX, touchY));
-//     off_btn.press(down && off_btn.contains touchX, touchY));
-//     if (on_btn.justReleased())
-//         on_btn.drawButton();
-//     if (off_btn.justReleased())
-//         off_btn.drawButton();
-//     if (on_btn.justPressed())
-//     {
-
-//         on_btn.drawButton(true);
-
-//         TFT.fillRect(40, 80, 160, 80, GREEN);
-//     }
-//     if (off_btn.justPressed())
-//     {
-//         off_btn.drawButton(true);
-//         TFT.fillRect(40, 80, 160, 80, RED);
-//     }
-// }
