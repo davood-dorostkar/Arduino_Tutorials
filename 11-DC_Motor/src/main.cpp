075b63ebc31bd70053ea1a13a53c1f5d979bf8a8
davood-dorostkar <dado2hacker@gmail.com>
2019-04-28 18:10:33 +0430

    stil does not work with larger motors--pot seems not working

--- 11-DC_Motor/src/main.cpp
+++ 11-DC_Motor/src/main.cpp
@@ -4,35 +4,35 @@
 #define input1Pin 8
 #define input2Pin 9
 
-// void forwardTurn(uint8_t speed)
-// {
-//   digitalWrite(input1Pin, HIGH);
-//   digitalWrite(input2Pin, LOW);
-//   analogWrite(enablePin, speed);
-// }
+void forwardTurn(uint8_t speed)
+{
+  digitalWrite(input1Pin, HIGH);
+  digitalWrite(input2Pin, LOW);
+  analogWrite(enablePin, speed);
+}
 
-// void backwardTurn(uint8_t speed)
-// {
-//   digitalWrite(input1Pin, LOW);
-//   digitalWrite(input2Pin, HIGH);
-//   analogWrite(enablePin, speed);
-// }
+void backwardTurn(uint8_t speed)
+{
+  digitalWrite(input1Pin, LOW);
+  digitalWrite(input2Pin, HIGH);
+  analogWrite(enablePin, speed);
+}
 
-// int motorDrive(uint8_t speed)
-// {
-//   if (speed < 550 && speed > 470)
-//     speed = 0;
-//   else if (speed <= 470)
-//   {
-//     speed = map(speed, 0, 512, 255, 0);
-//     forwardTurn(speed);
-//   }
-//   else
-//   {
-//     speed = map((speed - 512), 0, 512, 0, 255);
-//     backwardTurn(speed);
-//   }
-// }
+int motorDrive(uint8_t speed)
+{
+  if (speed < 550 && speed > 470)
+    speed = 0;
+  else if (speed <= 470)
+  {
+    speed = map(speed, 0, 512, 255, 0);
+    forwardTurn(speed);
+  }
+  else
+  {
+    speed = map((speed - 512), 0, 512, 0, 255);
+    backwardTurn(speed);
+  }
+}
 
 void setup()
 {
@@ -43,12 +43,6 @@ void setup()
 
 void loop()
 {
-  // uint8_t speed = analogRead(potentiometerPin);
-  int speed=50;
-  // motorDrive(speed);
-  // forwardTurn(map(speed,0,1024,0,255));
-
-  digitalWrite(input1Pin, HIGH);
-  digitalWrite(input2Pin, LOW);
-  analogWrite(enablePin, speed);
+  uint8_t speed = analogRead(potentiometerPin);
+  motorDrive(speed);
 }
