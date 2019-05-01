6f25edf7ed4e03571a6639781973508d16fe172f
davood-dorostkar <dado2hacker@gmail.com>
2019-05-01 15:10:16 +0430

    ok but brake not working

--- 11-DC_joystick/src/main.cpp
+++ 11-DC_joystick/src/main.cpp
@@ -21,26 +21,72 @@ void setup()
   pinMode(joystickY, INPUT);
 }
 
-void goAhead() {
-  digitalWrite(enableMotor1,HIGH);
-  digitalWrite(input1Motor1,HIGH);
-  digitalWrite(input2Motor1,LOW);
-
-  digitalWrite(enableMotor2,HIGH);
-  digitalWrite(input1Motor2,HIGH);
-  digitalWrite(input2Motor2,LOW);
+void motor1ForwardDrive(int speed)
+{
+  analogWrite(enableMotor1, speed);
+  digitalWrite(input1Motor1, HIGH);
+  digitalWrite(input2Motor1, LOW);
 }
-
-
-
+void motor2ForwardDrive(int speed)
+{
+  analogWrite(enableMotor2, speed);
+  digitalWrite(input1Motor2, HIGH);
+  digitalWrite(input2Motor2, LOW);
+}
+void motor1BackwardDrive(int speed)
+{
+  analogWrite(enableMotor1, speed);
+  digitalWrite(input1Motor1, LOW);
+  digitalWrite(input2Motor1, HIGH);
+}
+void motor2BackwardDrive(int speed)
+{
+  analogWrite(enableMotor2, speed);
+  digitalWrite(input1Motor2, LOW);
+  digitalWrite(input2Motor2, HIGH);
+}
+void motor1Brake()
+{
+  digitalWrite(enableMotor1, HIGH);
+  digitalWrite(input1Motor1, HIGH);
+  digitalWrite(input2Motor1, HIGH);
+}
+void motor2Brake()
+{
+  digitalWrite(enableMotor2, HIGH);
+  digitalWrite(input1Motor2, HIGH);
+  digitalWrite(input2Motor2, HIGH);
+}
+void motionPlan(int speedMotor1, int speedMotor2)
+{
+  if (speedMotor1 > 550)
+  {
+    speedMotor1 = map(speedMotor1, 550, 1024, 0, 255);
+    motor1ForwardDrive(speedMotor1);
   }
-  driveAmplitude= map(driveAmplitude,0,1024,0,255);
-  turnAmplitude= map(turnAmplitude,0,1024,0,255);
-
+  else if (speedMotor1 < 460)
+  {
+    speedMotor1 = map(speedMotor1, 460, 0, 0, 255);
+    motor1BackwardDrive(speedMotor1);
+  }
+  // else
+  //   motor1Brake();
+  if (speedMotor2 > 550)
+  {
+    speedMotor2 = map(speedMotor2, 550, 1024, 0, 255);
+   motor2ForwardDrive(speedMotor2);
+  }
+  else if (speedMotor2 < 460)
+  {
+    speedMotor2 = map(speedMotor2, 460, 0, 0, 255);
+    motor2BackwardDrive(speedMotor2);
+  }
+  // else
+  //   motor2Brake();
 }
 void loop()
 {
-  uint16_t driveAmplitude = analogRead(joystickX);
-  uint16_t turnAmplitude = analogRead(joystickY);
-
+  int speedMotor1 = analogRead(joystickX);
+  int speedMotor2 = analogRead(joystickY);
+  motionPlan(speedMotor1, speedMotor2);
 }
