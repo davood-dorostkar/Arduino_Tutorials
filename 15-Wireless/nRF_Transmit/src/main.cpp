5a44057314b0bb8fca5e1e94d453a58145d36ec3
davood-dorostkar <dado2hacker@gmail.com>
2019-06-24 16:11:05 +0430

    simplified nrf, without any addressing

--- 15-Wireless/nRF_Transmit/src/main.cpp
+++ 15-Wireless/nRF_Transmit/src/main.cpp
@@ -1,20 +1,24 @@
 #include <Arduino.h>
+/*
+SanatBazar
+Arduino Tutorial Series
+Author: Davood Dorostkar
+Website: www.sanatbazar.com
+
+*/
 
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 wifiSender(7, 8);
 const byte address[] = "00001";
-String amplifications[] = {"RF24_PA_MIN", "RF24_PA_LOW", "RF24_PA_HIGH", "RF24_PA_MAX"};
-int ampState = 0;
 
 void setup()
 {
     Serial.begin(9600);
     wifiSender.begin();
     Serial.println("Initializing the wifi... ");
-    Serial.println(amplifications[ampState]);
-    wifiSender.setPALevel(amplifications[ampState]);
-    Serial.println("The Amplification is set to: " + String(amplifications[ampState]));
+    wifiSender.setPALevel(RF24_PA_MIN);
+    Serial.println("The Amplification is set to: " + String(wifiSender.getPALevel()));
     wifiSender.openWritingPipe(address);
     wifiSender.stopListening();
     Serial.println("wifi initialized!");
@@ -22,11 +26,7 @@ void setup()
 
 void loop()
 {
-    while (!Serial.available())
-    {
-    }
-    String message = Serial.readStringUntil("\n");
-    // const char message[] = String(message);
+    char message[] = "I am sending something!";
     wifiSender.write(&message, sizeof(message));
     delay(2000);
 }
