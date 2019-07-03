f60b9f0830f81b803ff9bf3a88472f87b9dd4747
davood-dorostkar <dado2hacker@gmail.com>
2019-07-03 14:58:27 +0430

    started bluetooth

--- 15-Wireless/nodeMCU/src/main.cpp
+++ 15-Wireless/nodeMCU/src/main.cpp
@@ -7,13 +7,15 @@ Website: www.sanatbazar.com
 
 */
 
-void setup() {
-pinMode(D4,OUTPUT);
+void setup()
+{
+    pinMode(D4, OUTPUT);
 }
 
-void loop() {
-digitalWrite(D4,HIGH);
-delay(500);
-digitalWrite(D4,LOW);
-delay(1000);
+void loop()
+{
+    digitalWrite(D4, HIGH);
+    delay(500);
+    digitalWrite(D4, LOW);
+    delay(1000);
 }
---
+++ 16-Bluetooth/Bluetooth_LED/src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+/*
+SanatBazar
+Arduino Tutorial Series
+Author: Davood Dorostkar
+Website: www.sanatbazar.com
+
+*/
+#include <SoftwareSerial.h>
+#define ledPin 5
+int state = 0;
+SoftwareSerial bluetooth(2, 3);
+
+void setup()
+{
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, state);
+}
+
+void loop()
+{
+  if (bluetooth.available() > 0)
+  {
+    int message = bluetooth.read();
+    message -= 48;
+    if (message != state)
+    {
+      state = message;
+      Serial.print("message: ");
+      Serial.println(message);
+      Serial.print("state: ");
+      Serial.println(state);
+      Serial.println("stat != message");
+      Serial.println();
+      digitalWrite(ledPin, state);
+    }
+  }
+}
