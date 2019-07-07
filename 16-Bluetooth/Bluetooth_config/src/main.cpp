3e66bba91c7dccc243e86fba540004a2f1c5afa1
davood-dorostkar <dado2hacker@gmail.com>
2019-07-07 12:04:35 +0430

    done with bluetooth

--- 16-Bluetooth/Bluetooth_config/src/main.cpp
+++ 16-Bluetooth/Bluetooth_config/src/main.cpp
@@ -13,18 +13,14 @@ SoftwareSerial bluetooth(2, 3); // RX | TX
 void setup()
 {
   Serial.begin(38400);
-  bluetooth.begin(38400);  //Baud Rate for command Mode. 
+  bluetooth.begin(38400);
   Serial.println("Enter AT commands!");
 }
  
 void loop()
 {
- 
-  // Feed any data from bluetooth to Terminal.
   if (bluetooth.available())
     Serial.write(bluetooth.read());
- 
-  // Feed all data from termial to bluetooth
   if (Serial.available())
     bluetooth.write(Serial.read());
 }
